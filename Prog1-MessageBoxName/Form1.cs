18e40f5ae78b757d181a9b3a762e9c630b85ae9c
RodFranz Nuque <rodkolehiyo@gmail.com>
2022-04-27 11:20:52 +0800

    First Commit

---
+++ Prog1-MessageBoxName/Form1.cs
@@ -0,0 +1,35 @@
+namespace Prog1_MessageBoxName
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("RodFranz F. Nuque");
+        }
+    }
+}
